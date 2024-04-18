f516e2393e27bfab33f4e81b798b2177d3236602
Lee <lee.jensen@agilix.com>
2024-04-18 09:55:29 -0600

    add(menu) basic functionality

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -22,38 +22,20 @@ export default function Home() {
 
       <Carousel
         slides={[
-          <Quote
-            quote='Most amazing pizza experience of my life.'
-            author='Megan Fox, Springville'
-          />,
+          <Quote quote='Most amazing pizza experience of my life.' author='Megan Fox, Springville' />,
           <Quote quote='Milan reborn!' author='张伟, Provo' />,
           <Quote quote='All I can say is WOW!' author='José García, Orem' />,
-          <Quote
-            quote='Best pizza ever. I can eat this every day!'
-            author='Terrence Jones, Mapleton'
-          />,
+          <Quote quote='Best pizza ever. I can eat this every day!' author='Terrence Jones, Mapleton' />,
         ]}
       />
 
       <div className='m-4 grid gap-x-8 gap-y-4 sm:gird-cols-1 md:grid-cols-2 lg:grid-cols-4 xlg:grid-cols-5'>
-        <Card
-          title='Springville'
-          description='The Springville store'
-          image='pizza1.png'
-        />
+        <Card title='Springville' description='The Springville store' image='pizza1.png' />
         <Card title='Orem' description='The Orem store' image='pizza2.png' />
         <Card title='Provo' description='The provo store' image='pizza3.png' />
-        <Card
-          title='Lindon'
-          description='The Lindon store'
-          image='pizza4.png'
-        />
+        <Card title='Lindon' description='The Lindon store' image='pizza4.png' />
         <Card title='Nephi' description='The Nephi store' image='pizza5.png' />
-        <Card
-          title='American Fork'
-          description='The American Fork store'
-          image='pizza6.png'
-        />
+        <Card title='American Fork' description='The American Fork store' image='pizza6.png' />
       </div>
     </div>
   );
--- src/views/menu.jsx
+++ src/views/menu.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import Card from '../components/card';
+import { useState } from 'react';
 
 export default function Menu() {
+  const [count, setCount] = useState(0);
+  const [orderMessage, setOrderMessage] = useState('');
+
+  function onClick() {
+    setCount(count + 1);
+  }
+
+  function onOrder() {
+    if (count === 0) setOrderMessage('Please select a pizza first. How about a Margarita?');
+    else if (count > 1) setOrderMessage(`Order placed for ${count} pizzas!`);
+    setCount(0);
+  }
+
+  const pizzas = [
+    { title: 'Veggie', description: 'A garden of delight', image: 'pizza1.png' },
+    { title: 'Pepperoni', description: 'Spicy treat', image: 'pizza2.png' },
+    { title: 'Margarita', description: 'Essential classic', image: 'pizza3.png' },
+    { title: 'Crusty', description: 'A dry mouthed favorite', image: 'pizza4.png' },
+    { title: 'Flat', description: 'Something special', image: 'pizza5.png' },
+    { title: 'Chared Leopard', description: 'For those with a darker side', image: 'pizza6.png' },
+  ];
   return (
-    <div className='text-neutral-100'>
-      Pizza, pizza, pizza, pizza, pizza, pizza, pizza, pizza, pizza, and more PIZZA!
+    <div className='flow flow-col justify-center text-neutral-100'>
+      <div className='my-2 sm:my-8 sm:text-yellow-200'>
+        Pizza, pizza, pizza, pizza, pizza, pizza, pizza, pizza, pizza, and more PIZZA!
+      </div>
+
+      <div> Selected pizzas: {count}</div>
+      <button
+        type='button'
+        className='w-32 my-4 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-orange-800 text-white hover:bg-orange-600'
+        onClick={onOrder}
+      >
+        Buy now
+      </button>
+      <div className='h-8'>{orderMessage}</div>
+
+      <div className='m-4 grid gap-x-8 gap-y-4 sm:gird-cols-1 md:grid-cols-2 lg:grid-cols-4 xlg:grid-cols-5'>
+        {pizzas.map((pizza) => (
+          <button type='button' onClick={onClick}>
+            <Card title={pizza.title} description={pizza.description} image={pizza.image} />
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
