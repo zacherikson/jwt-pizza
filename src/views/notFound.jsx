877bead9d92e730cd800a7c43698c6042a9f624c
Lee <lee.jensen@agilix.com>
2024-04-19 10:01:21 -0600

    fix() dead code

--- src/views/notFound.jsx
+++ src/views/notFound.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 
 export default function NotFound() {
   return (
-    <div className='text-neutral-100'>
-      It looks like we have dropped a pizza on the floor. Please try another
-      page.{' '}
-    </div>
+    <div className='text-neutral-100'>It looks like we have dropped a pizza on the floor. Please try another page.</div>
   );
 }
