1d42ae338ef33244e8abfa554d1e23a519a05a2f
Lee <leesjensen@gmail.com>
2024-04-19 15:46:49 -0600

    add(login) show password

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PersonIcon, EmailIcon } from '../icons';
+import { CloseEyeIcon, PersonIcon, EmailIcon } from '../icons';
 import Button from '../components/button';
 
 export default function Login() {
@@ -36,13 +36,17 @@ export default function Login() {
           </label>
           <div className='relative'>
             <input
+              id='password'
               type='password'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              id='hs-cover-with-gradient-form-name-1'
               className='py-3 ps-11 pe-4 block w-full bg-white/10 border-white/20 text-white placeholder:text-white rounded-lg text-sm focus:border-white/30 focus:ring-white/30 sm:p-4 sm:ps-11'
               placeholder='Password'
             />
+            <button type='button' data-hs-toggle-password='{"target": "#password"}' className='absolute top-0 end-0 p-4 rounded-e-md'>
+              <CloseEyeIcon />
+            </button>
+
             <div className='absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4'>
               <PersonIcon />
             </div>
