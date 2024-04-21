23fbf5c37ff2e5c42aa4c3e7fd79249f2d1ccafb
Lee <leesjensen@gmail.com>
2024-04-20 20:15:53 -0600

    cleanup(menu) with simplified interface

--- src/views/menu.jsx
+++ src/views/menu.jsx
@@ -7,7 +7,6 @@ import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function Menu() {
   const [count, setCount] = useState(useLocation().state?.orderCount || 0);
-  const [orderMessage, setOrderMessage] = useState('');
   const navigate = useNavigate();
 
   function onClick() {
@@ -15,11 +14,7 @@ export default function Menu() {
   }
 
   function onCheckout() {
-    if (count === 0) {
-      setOrderMessage('Please select a pizza first. How about a Margarita?');
-    } else if (count > 0) {
-      setOrderMessage(`Order placed for ${count} pizzas!`);
-      setCount(0);
+    if (count > 0) {
       navigate('/payment', { state: { orderCount: count } });
     }
   }
@@ -35,11 +30,10 @@ export default function Menu() {
   return (
     <View title='Awesome is on the way'>
       <div className='flow flow-col justify-center text-neutral-100'>
-        <div className='my-2 sm:my-8 sm:text-yellow-200'>Pick your pizza from below. Remember to order extra for a midnight party.</div>
+        <div className='my-2 sm:my-8'>Pick your pizza from below. Remember to order extra for a midnight party.</div>
 
-        <div> Selected pizzas: {count}</div>
-        <Button title='Checkout' onPress={onCheckout} />
-        <div className='h-8'>{orderMessage}</div>
+        <div className='text-yellow-200'>{count > 0 ? 'Selected pizzas: ' + count : 'What are you waiting for? Pick a pizza!'}</div>
+        <Button title='Checkout' onPress={onCheckout} disabled={count <= 0} className='disabled' />
 
         <div className='m-4 grid gap-x-8 gap-y-4 sm:gird-cols-1 md:grid-cols-2 lg:grid-cols-4 xlg:grid-cols-5'>
           {pizzas.map((pizza) => (
