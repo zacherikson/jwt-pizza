e0e1051d0ea53e75a5a39f56628db7d7db0b982b
Lee <leesjensen@gmail.com>
2024-04-20 21:34:48 -0600

    refactor(api)

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,32 @@
 const roles = ['admin', 'dinner', 'franchise'];
 
+const pizzas = [
+  { title: 'Veggie', description: 'A garden of delight', image: 'pizza1.png' },
+  { title: 'Pepperoni', description: 'Spicy treat', image: 'pizza2.png' },
+  { title: 'Margarita', description: 'Essential classic', image: 'pizza3.png' },
+  { title: 'Crusty', description: 'A dry mouthed favorite', image: 'pizza4.png' },
+  { title: 'Flat', description: 'Something special', image: 'pizza5.png' },
+  { title: 'Chared Leopard', description: 'For those with a darker side', image: 'pizza6.png' },
+];
+
+const purchases = [
+  { name: 'Pepperoni', price: 35, date: new Date('2024-03-10T00:00:00Z') },
+  { name: 'Veggie', price: 50, date: '2024-03-10T00:00:00Z' },
+  { name: 'Margarita', price: 45, date: '2024-03-10T00:00:00Z' },
+];
+
+const franchiseStores = [
+  { name: 'Orem', totalRevenue: 3000000, address: '234 N 300 S' },
+  { name: 'Provo', totalRevenue: 53000, address: '234 N 300 S' },
+  { name: 'Payson', totalRevenue: 458767832, address: '234 N 300 S' },
+];
+
+const franchises = [
+  { name: 'SuperPie', totalRevenue: 3000000, franchisee: 'joe@franchise.com' },
+  { name: 'LotaPizza', totalRevenue: 53000, franchisee: 'moe@franchise.com' },
+  { name: 'PizzaCorp', totalRevenue: 458767832, franchisee: 'berry@franchise.com' },
+];
+
 class Api {
   isAdmin(user) {
     return user?.role === 'admin';
@@ -10,62 +37,41 @@ class Api {
   }
 
   async getPizzas() {
-    const pizzas = [
-      { title: 'Veggie', description: 'A garden of delight', image: 'pizza1.png' },
-      { title: 'Pepperoni', description: 'Spicy treat', image: 'pizza2.png' },
-      { title: 'Margarita', description: 'Essential classic', image: 'pizza3.png' },
-      { title: 'Crusty', description: 'A dry mouthed favorite', image: 'pizza4.png' },
-      { title: 'Flat', description: 'Something special', image: 'pizza5.png' },
-      { title: 'Chared Leopard', description: 'For those with a darker side', image: 'pizza6.png' },
-    ];
-
     return pizzas;
   }
 
   async getPurchases(purchasingUser) {
-    let purchases = [];
+    let result = [];
 
     if (purchasingUser) {
       const user = await this.getUser();
       if (user && (this.isAdmin(user) || user.email === purchasingUser.email)) {
-        purchases = [
-          { name: 'Pepperoni', price: 35, date: new Date('2024-03-10T00:00:00Z') },
-          { name: 'Veggie', price: 50, date: '2024-03-10T00:00:00Z' },
-          { name: 'Margarita', price: 45, date: '2024-03-10T00:00:00Z' },
-        ];
+        result = purchases;
       }
     }
-    return purchases;
+    return result;
   }
 
   async getFranchiseStores(franchiseUser) {
-    let franchiseStores = [];
+    let result = [];
 
     if (franchiseUser) {
       const user = await this.getUser();
       if (this.isFranchisee(user) || this.isAdmin(user)) {
-        franchiseStores = [
-          { name: 'Orem', totalRevenue: 3000000, address: '234 N 300 S' },
-          { name: 'Provo', totalRevenue: 53000, address: '234 N 300 S' },
-          { name: 'Payson', totalRevenue: 458767832, address: '234 N 300 S' },
-        ];
+        result = franchiseStores;
       }
     }
-    return franchiseStores;
+    return result;
   }
 
   async getFranchises() {
-    let franchises = [];
+    let result = [];
 
     const user = await this.getUser();
     if (this.isAdmin(user)) {
-      franchises = [
-        { name: 'SuperPie', totalRevenue: 3000000, franchisee: 'joe@franchise.com' },
-        { name: 'LotaPizza', totalRevenue: 53000, franchisee: 'moe@franchise.com' },
-        { name: 'PizzaCorp', totalRevenue: 458767832, franchisee: 'berry@franchise.com' },
-      ];
+      result = franchises;
     }
-    return franchises;
+    return result;
   }
 
   async login(email, password) {
