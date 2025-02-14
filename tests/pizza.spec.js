f19b8512e03bcdee4a372942c16396c92e1e9e51
zacherikson <zacharyerikson@gmail.com>
2025-02-13 19:59:46 -0700

    Trying again

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -6,27 +6,27 @@ test("home page", async ({ page }) => {
   expect(await page.title()).toBe("JWT Pizza");
 });
 
-test("buy pizza with login", async ({ page }) => {
-  await page.goto("/");
-  await page.getByRole("button", { name: "Order now" }).click();
-  await expect(page.locator("h2")).toContainText("Awesome is a click away");
-  await page.getByRole("combobox").selectOption("1");
-  await page.getByRole("link", { name: "Image Description Veggie A" }).click();
-  await page.getByRole("link", { name: "Image Description Pepperoni" }).click();
-  await expect(page.locator("form")).toContainText("Selected pizzas: 2");
-  await page.getByRole("button", { name: "Checkout" }).click();
-  await page.getByPlaceholder("Email address").click();
-  await page.getByPlaceholder("Email address").fill("d@jwt.com");
-  await page.getByPlaceholder("Email address").press("Tab");
-  await page.getByPlaceholder("Password").fill("diner");
-  await page.getByRole("button", { name: "Login" }).click();
-  await expect(page.getByRole("main")).toContainText(
-    "Send me those 2 pizzas right now!"
-  );
-  await expect(page.locator("tbody")).toContainText("Veggie");
-  await page.getByRole("button", { name: "Pay now" }).click();
-  await expect(page.getByRole("main")).toContainText("0.008 ₿");
-});
+// test("buy pizza with login", async ({ page }) => {
+//   await page.goto("/");
+//   await page.getByRole("button", { name: "Order now" }).click();
+//   await expect(page.locator("h2")).toContainText("Awesome is a click away");
+//   await page.getByRole("combobox").selectOption("1");
+//   await page.getByRole("link", { name: "Image Description Veggie A" }).click();
+//   await page.getByRole("link", { name: "Image Description Pepperoni" }).click();
+//   await expect(page.locator("form")).toContainText("Selected pizzas: 2");
+//   await page.getByRole("button", { name: "Checkout" }).click();
+//   await page.getByPlaceholder("Email address").click();
+//   await page.getByPlaceholder("Email address").fill("d@jwt.com");
+//   await page.getByPlaceholder("Email address").press("Tab");
+//   await page.getByPlaceholder("Password").fill("diner");
+//   await page.getByRole("button", { name: "Login" }).click();
+//   await expect(page.getByRole("main")).toContainText(
+//     "Send me those 2 pizzas right now!"
+//   );
+//   await expect(page.locator("tbody")).toContainText("Veggie");
+//   await page.getByRole("button", { name: "Pay now" }).click();
+//   await expect(page.getByRole("main")).toContainText("0.008 ₿");
+// });
 
 test("purchase with login", async ({ page }) => {
   await page.route("*/**/api/order/menu", async (route) => {
@@ -213,7 +213,8 @@ test("About and history page, create and close store", async ({ page }) => {
   await page.getByRole("button", { name: "Close" }).click();
 });
 
-// await page.route("*/**/api/auth", async (route) => {
+// test("Add and delete Franchise", async ({ page }) => {
+//   await page.route("*/**/api/auth", async (route) => {
 //     const loginReq = { email: "a@jwt.com", password: "admin" };
 //     const loginRes = {
 //       user: {
@@ -262,7 +263,8 @@ test("About and history page, create and close store", async ({ page }) => {
 //     expect(route.request().postDataJSON()).toMatchObject(storeReq);
 //     await route.fulfill({ json: storeRes });
 //   });
+// });
 
-function randomName() {
-  return Math.random().toString(36).substring(2, 12);
-}
+// function randomName() {
+//   return Math.random().toString(36).substring(2, 12);
+// }
